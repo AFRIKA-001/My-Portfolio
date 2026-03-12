356a6215aa55e6b3e865cd2db6ee802f36b31191
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-03-12 16:52:00 +0300

    added svg icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,15 @@
 export default function Skills(){
     return<>
     <section>
-  <h1>My Skills</h1>
-  
+  <h1>Technical Skills</h1>
+  <p>here are some of the tools and tecnologies i use to bring ideas to life </p>
+  <div>
+   <div>
+    <img src="" alt="" />
+    <h3>React.js</h3>
+    </div>
+
+  </div>
     </section>
   
     </>
