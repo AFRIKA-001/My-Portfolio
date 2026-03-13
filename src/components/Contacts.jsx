731220b4b84e071b38b1a18464dd6cd3830001e2
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-03-13 12:14:18 +0300

    added the the form and contacts icons

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,43 @@
+export default function Coontacts(){
+    return<>
+     <section>
+        
+            <h1>Get In Touch</h1>
+
+            <div>
+                <h1>Let's Talk</h1>
+                <p> 
+                    I'm currently accepting new freelance opportunities.
+                     If you're looking for a dedicated developer to help with your next build,
+                      I’d love to hear from you. Feel free to send a message anytime to get started
+                </p>
+                <p></p>
+                <p></p>
+
+
+            </div>
+            {/* input filed starts here */}
+            <div> 
+            
+                <label>Your Name</label>
+                <input type="text" />
+
+
+                <label>Your Email</label>
+                <input type="email" />
+
+
+                <label>Message</label>
+                <input type="message" />
+
+
+            </div>
+            {/* input fields ends here */}
+        
+
+    </section>
+    
+
+           
+    </>
+}
