1dc7daa80d6f252af8bd6ad6b4f3ea8711f46da4
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-10 22:41:20 +0300

    added the footer to the rootlayout in order to be rendered in every page

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavigationBar from './NavigationBar'
+import Footer from './footer.jsx'
 function RootLayout() {
   return (
     <div>
       <NavigationBar />
       <Outlet />
+       <Footer />
     </div>
   )
 }
