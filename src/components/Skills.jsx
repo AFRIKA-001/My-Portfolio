98fe7c259e7a4cf5bb34894cbab25e1f5001fb81
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-04-12 17:47:13 +0300

    changed the padding of the skill cards on smaller screens

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,7 +17,7 @@ export default function Skills() {
         <section className="pt-15 bg-[#0B1120]">
                  <h1 className="lg:text-6xl text-2xl text-center mb-10 font-bold  text-transparent bg-clip-text bg-gradient-to-l from-cyan-500 to-indigo-600">Technical Skills</h1>
             <p className="text-xl  text-w-2xl text-center  text-white font-medium leading-relaxed">here are some of the tools and tecnologies i use to bring ideas to life </p>
-            <div className=" grid grid-cols-2 lg:grid-cols-3 pt-10 items-center gap-10 pl-6 m-auto lg:pl-60">
+            <div className=" grid grid-cols-2 lg:grid-cols-3 pt-10 items-center gap-10 pl-2 m-auto lg:pl-60">
 
                 <div className="flex justify-center h-30 w-40 lg:w-55 items-center hover:-translate-y-4 hover:shadow-xl hover:border-cyan-400 rounded bg-[#1E1E1E] border border-white/10 shadow-cyan-200/20 transition-all duration-500 ">
                     <img className="w-10 h-10 text-blue-700 object-contain" src={ReactIcon} alt="react icon" />
