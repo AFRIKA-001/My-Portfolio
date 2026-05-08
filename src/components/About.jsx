7d090ac6c4564bf8f5281a836a97846fa85c2f52
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-05-08 23:49:51 +0300

    removed the backdrop color and the border bottom and border right

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@
            <h1 className='lg:text-6xl text-4xl font-serif text-center mb-6 lg:mb-20   text-transparent bg-clip-text bg-gradient-to-l from-cyan-500 to-indigo-600'><span className='font-bold lg:text-8xl'>A</span>bout Me</h1>
            <div className=' my-4 lg:my-10  grid grid-cols-1 md:grid-cols-2  items-center'>
         <img  className=' flex mx-auto rounded-full w-50 h-50 lg:w-70 lg:h-70 aspect-square object-cover 'src={ProfilePic} alt="Gabriel's logo" />
-        <p className='text-xl flex text-center font-serif lg:pr-9 pt-10 text-white font-medium leading-relaxed backdrop-blur-md bg-white/10 border-b border-white border-r'>
+        <p className='text-xl flex text-center font-serif lg:pr-9 pt-10 text-white font-medium leading-relaxed   border-white '>
             Hi! I'm Gabriel. I am a Computer Science student and a self-driven Frontend Developer currently specializing in the MERN stack. My development philosophy is rooted in project-based learning. I believe the best way to master a tool like React is by building real-world solutions.
 
             I spend my time exploring the latest in web performance and Cybersecurity, ensuring that the sites I build aren't just pretty, but secure and optimized. I'm always looking for new challenges that push me to write cleaner, more efficient code.
