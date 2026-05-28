18856f50ac5c1780c8d38b20c28fdbdca0112410
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-05-28 13:01:11 +0300

    set up Toaster globally with own styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './components/Routes.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
+    <Toaster position='top-center' reverseOrder={false} toastOptions={{
+      duration:2000,
+      style:{
+        background:"#fff",
+        color:"#000",
+        height:"50%",
+        width:"80%",
+        border:"1px",
+        borderRadius:"10px"
+      }
+    }} />
   </StrictMode>,
 )
