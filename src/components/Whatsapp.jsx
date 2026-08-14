7c9ebbeeead3a40e3523511d32364d3f19579aad
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-08-14 15:04:29 +0300

    applied stylet to the whatsapp icon

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -3,12 +3,22 @@ import { BsChat } from 'react-icons/bs'
 
 const Whatsapp = () => {
   return (
-    <div className=' bg-linear-to-r from-green-500 via-green-600 to-green-700 px-6 py-3 fixed bottom-8 right-8 overflow-hidden'>
-        <a href="https://wa.me/254714058073" rel='noopener noreferrer' target='_blank' className='flex items-center'>
-        <BsChat size={34} className='text-white'></BsChat>
+    <>
+    <section>
+    <div className="bg-red-600 fixed bottom-8 right-8 z-50 animate-ping w-[1em] h-[1em] rounded-full"></div>
+
+    <div className=' bg-linear-to-r from-green-500 via-green-600 to-green-700 px-6 py-3 fixed rounded-2xl bottom-8 right-8 overflow-hidden'>
+      <a href="https://wa.me/254714058073" rel='noopener noreferrer' target='_blank' className='flex items-center gap-3'>
+        <BsChat size={34} className='text-white '></BsChat>
         Chat
-        </a>
+      </a>
+
     </div>
+
+    </section>
+    
+   
+    </>
   )
 }
 
