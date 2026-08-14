291d164aecb38515a1b303fe7c10ff6a4bfd1d14
AFRIKA-001 <shikukugabriel06@gmail.com>
2026-08-14 15:06:28 +0300

    added the Whatsapp icon to all pages

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavigationBar from './NavigationBar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavigationBar from "./NavigationBar";
+import Whatsapp from "./Whatsapp.jsx";
 
 function RootLayout() {
   return (
     <div>
       <NavigationBar />
       <Outlet />
-   
+      <Whatsapp />
     </div>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;
